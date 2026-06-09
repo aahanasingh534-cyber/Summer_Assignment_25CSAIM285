65fd112a339e607deeec0ddb8a823e50a8528012
aahanasingh534 <aahanasingh534@gmail.com>
2026-06-09 16:21:33 +0530

    day 9 programs uploaded

---
+++ day 9/prog4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("input the no of rows:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i==1||i==5||j==1||j==5){
+            printf("*");
+            }
+            else{
+            printf(" ");
+        }
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
